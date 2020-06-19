4bff14d6fc681027a4f6948f9e8d683216101f03
Omar Saldanna <node.omar.web@gmail.com>
2020-06-19 18:59:50 -0500

    New functionality to buttons

--- index.js
+++ index.js
@@ -13,18 +13,29 @@ const buttons = [ btn1, btn2, btn3, btn4 ];
 // the shadows colors
 const shadows = [ '#00C1FF' , '#2EFF7B', '#FF0A27', '#9A63FF' ];
 
+// the light background colors and normal colors
+const lightColors = [ '#4bc7f7', '#34ff80', '#ff1631', '#8d5de8' ];
+const defaultColors = [ '#00acee', '#25d366', '#bd081c', '#6441a5' ];
+
 // a function for play the buttos automaticly
 async function pressBtn(btn) {
   // select te button
   let target = buttons[btn-1];
-  // and the shadow
+  // the shadow color
   let shadow = shadows[btn-1];
-  // change the box shadow
+  // the light color
+  let lightColor = lightColors[btn-1];
+  // and the default color
+  let defaultColor = defaultColors[btn-1];
+
+  // change the box shadow and background to a light color
   target.style.boxShadow = `0px 0px 20px -1px ${shadow}`;
+  target.style.backgroundColor = lightColor;
   // play the press sound
   sound.play();
-  // wait and change the shadow
+  // wait and change the shadow and background to default
   await setTimeout(() => {
     target.style.boxShadow = 'none';
+    target.style.backgroundColor = defaultColor;
   }, 500);
 }
